b4b582149c7b2855ebcc1ac516bf6346118d5fa3
euteryu <54965926+euteryu@users.noreply.github.com>
2023-12-20 00:15:55 +0000

    Update RIE-MSCA-3000.py
    Changed save location dir path

--- RIE-MSCA-3000.py
+++ RIE-MSCA-3000.py
@@ -18,21 +18,26 @@ def find_path_GUI():
 def extract_name(filepath):
 	ics_filename = filepath.split("/")[-1]
 	ics_filename = re.findall(r'\s|,|[^,\s]+', ics_filename)
-	csv_filename = ''.join(ics_filename[-1] + ics_filename[0] + ".csv")
-	print(csv_filename)
+	csv_filename = ''.join(ics_filename[0] + ics_filename[-1])
 	return csv_filename
+
+# Column B | 2023-10-06 15:00:00+00:00 --> 
+# Column C | 2024-02-01 14:00:00+00:00
+def csvToGrid():
+	pass
 
 def main():
 	convert = csv_ical.Convert()
 
 	convert.ICS_FILE_LOCATION = find_path_GUI()
-	csv_dir = "~/Python/RIE-MSCA-3000/resources/csv_files"
+	csv_dir = "/home/minseok/Python/RIE-MSCA-3000/resources/csv_files"
 	csv_filename = extract_name(convert.ICS_FILE_LOCATION)
 	convert.CSV_FILE_LOCATION = os.path.join(csv_dir, csv_filename)
 
 	convert.read_ical(convert.ICS_FILE_LOCATION)
 	convert.make_csv()
 	convert.save_csv(convert.CSV_FILE_LOCATION)
+	# convert.save_csv('~/Python/RIE-MSCA-3000/resources/csv_files/RYUMinseok.ics')
 
 if __name__=="__main__":
 	main()
