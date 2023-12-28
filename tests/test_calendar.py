530b6f339172a8f796c07d1cbeedac684de2eb8c
euteryu <minseok5432@gmail.com>
2023-12-28 16:20:52 +0000

    Tested merge of two different files

---
+++ tests/test_calendar.py
@@ -0,0 +1,3 @@
+# RESOURCES
+# https://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure
+# https://www.geeksforgeeks.org/typical-directory-structure-for-running-tests-using-unittest-in-python/
